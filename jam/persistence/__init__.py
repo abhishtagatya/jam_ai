cf438b76de64539bbf7b70730c52301cd1f610b3
abhishtagatya <abhishtagatya@yahoo.com>
2023-06-27 19:44:02 +0700

    Add exception for optional dependencies on init

--- jam/persistence/__init__.py
+++ jam/persistence/__init__.py
@@ -1,3 +1,8 @@
 from jam.persistence.memory import MemoryPersistence
 from jam.persistence.sqlite import SQLitePersistence
-from jam.persistence.postgres import PostgresPersistence
+
+try:
+    from jam.persistence.postgres import PostgresPersistence
+    from jam.persistence.mysql import MySQLPersistence
+except ModuleNotFoundError as mn_err:
+    pass
